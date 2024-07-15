ad289286c908b4b87504442afe4b79370a8cb8e3
Ali Naqvi <ali.naqvi8062@gmail.com>
2024-07-15 16:10:20 -0400

    Configure SysTick timer to 1ms using default HSI clock (16MHz)

--- Src/main.c
+++ Src/main.c
@@ -17,6 +17,9 @@ void task2_handler(void);
 void task3_handler(void);
 void task4_handler(void);
 
+
+void init_systick_timer(uint32_t tick_hz);
+
 #define SIZE_TASK_STACK			1024U // arbitrarily allocating 1kb space to each task
 #define SIZE_SCHED_STACK		1024U
 
@@ -30,9 +33,14 @@ void task4_handler(void);
 #define T4_STACK_START			((SRAM_END) - (3 * SIZE_TASK_STACK))
 #define SCHED_STACK_START		((SRAM_END) - (4 * SIZE_TASK_STACK))
 
+#define TICK_HZ 1000U
+#define HSI_CLOCK				16000000U
+#define SYSTICK_TIM_CLK			HSI_CLOCK
+
 int main(void)
 {
     /* Loop forever */
+	init_systick_timer(TICK_HZ);
 	for(;;);
 }
 
@@ -60,6 +68,36 @@ void task4_handler(void){
 	while(1){
 		printf("This is task 4\n");
 	}
+}
+
+void init_systick_timer(uint32_t tick_hz){
+
+	uint32_t *pSRVR = (uint32_t*)0xE000E014; //SysTick reload value register, SVR
+	uint32_t *pSCSR = (uint32_t*)0xE000E010; //SysTick Control and Status register
+
+
+	uint32_t count_value = (SYSTICK_TIM_CLK/tick_hz)-1; //1ms
+
+	//clear the value of SVR
+	*pSRVR &= ~(0x00FFFFFFFF);
+
+	//load the value in to SVR
+	*pSRVR |= count_value;
+
+	//enable SysTick exception request
+	*pSCSR |= (1<<1);
+
+	//indicate processor clock source
+	*pSCSR |= (1<<2);
+
+	//enable systick counter
+	*pSCSR |= (1<<0);
+
+}
+
+void SysTick_Handler(void)
+{
+	//context switching
 }
 
 
