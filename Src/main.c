58cfcb0b70812dd723094854c15e7a3f5c429ace
Ali Naqvi <ali.naqvi8062@gmail.com>
2024-07-15 15:35:15 -0400

    Allocate stack memory and define start/end positions

--- Src/main.c
+++ Src/main.c
@@ -11,11 +11,24 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-// Function prototypes
+// Task prototypes
 void task1_handler(void);
 void task2_handler(void);
 void task3_handler(void);
 void task4_handler(void);
+
+#define SIZE_TASK_STACK			1024U // arbitrarily allocating 1kb space to each task
+#define SIZE_SCHED_STACK		1024U
+
+#define SRAM_START				0x20000000U
+#define SIZE_SRAM				((128) * (1024))
+#define SRAM_END				((SRAM_START) + (SIZE_SRAM))
+
+#define T1_STACK_START			SRAM_END
+#define T2_STACK_START			((SRAM_END) - (1 * SIZE_TASK_STACK))
+#define T3_STACK_START			((SRAM_END) - (2 * SIZE_TASK_STACK))
+#define T4_STACK_START			((SRAM_END) - (3 * SIZE_TASK_STACK))
+#define SCHED_STACK_START		((SRAM_END) - (4 * SIZE_TASK_STACK))
 
 int main(void)
 {
